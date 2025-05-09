67f3fa40078ce0e4f1b1665a7ab89519785441ed
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-05-08 22:08:16 -0600

    I did the assignation

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine(" What is your fist name? ");
+        string name = Console.ReadLine() ; 
+
+        Console.WriteLine("What is your last name? ") ;
+        string lastname = Console.ReadLine() ;
+
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}."); 
     }
 }
+
